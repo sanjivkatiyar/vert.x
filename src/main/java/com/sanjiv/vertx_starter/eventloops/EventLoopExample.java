2e5e43f160ea7ac0d2f559fc4b4f4930d002e2e5
sanjiv <sanjiv.katiyar@gmail.com>
2022-09-26 14:31:39 -0700

    ok

---
+++ src/main/java/com/sanjiv/vertx_starter/eventloops/EventLoopExample.java
@@ -0,0 +1,32 @@
+package com.sanjiv.vertx_starter.eventloops;
+
+import com.sanjiv.vertx_starter.verticles.MainVerticle;
+import io.vertx.core.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class EventLoopExample extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(EventLoopExample.class);
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx(
+      new VertxOptions()
+        .setMaxEventLoopExecuteTime(500)
+        .setMaxEventLoopExecuteTimeUnit(TimeUnit.MILLISECONDS)
+        .setBlockedThreadCheckInterval(1)
+        .setBlockedThreadCheckIntervalUnit(TimeUnit.SECONDS)
+    );
+    vertx.deployVerticle(EventLoopExample.class.getName(),
+      new DeploymentOptions().setInstances(4));
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    LOG.debug("Start {}", this.getClass().getName());
+    startPromise.complete();
+    Thread.sleep(5000);
+  }
+}
